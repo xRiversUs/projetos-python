8c8628ba6c446a98bb8ce2f0ef130b4c74028b6f
xRiversUs <thmrivers@gmail.com>
2026-05-04 16:09:19 +0000

    v2

---
+++ quizz.py
@@ -0,0 +1,6 @@
+print("#--Quiz de astronomia--")
+resposta = input("Qual é o maior planeta do Sistema Solar ☀️? \n a- Júpiter \n b- Saturno \n c- Urano \n").lower().strip()
+if resposta =="a":
+   print("Resposta correta 👌")
+else:
+   print("Resposta incorreta 👎")
--- seletor.py
+++ seletor.py
@@ -1,8 +1,9 @@
-#--Seletor de caracteres--
-letra = input("Digite a letra")
+print("#--Seletor de caracteres--🔠")
+print("Escolha uma letra e veja se ela está disponível 👀")
+letra = input("Digite a letra: ").upper()
 if letra == "A":
-  print("Você escolheu a letra A")
+  print("Você escolheu a letra A 👌")
 elif letra == "B":
-  print("Você escolheu a letra B")
+  print("Você escolheu a letra B 👌")
 else:
-  print("Essa opção não está disponível")
+  print("Essa opção não está disponível 👎")
