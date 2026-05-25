2e78174b653dd90f78e1a5e8ed9fb4ff4ccb457a
xRiversUs <thmrivers@gmail.com>
2026-05-25 12:18:19 +0000

    v1

---
+++ cabeçalho.py
@@ -0,0 +1,10 @@
+### Definindo e Chamando a Função
+def exibir_cabecalho():
+    print('=' * 30)
+    print('SISTEMA DE GESTÃO ESCOLAR')
+    print('Versão 1.0 - Desenvolvimento no Codespace')
+    print('=' * 30)
+
+### Chamado a função
+exibir_cabecalho()
+print('Menu Principal')
